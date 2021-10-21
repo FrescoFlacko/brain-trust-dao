73538da285a1d3e9cc914b44b78e5eda83c1ac9a
Yandre <yandre3000@protonmail.com>
2021-10-21 19:40:44 -0400

    Added governance static folder

---
+++ governance/precache-manifest.293e9298a052118937dfa39205781873.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b202bad86dcfd17be5e9b48048ef72a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c2137958604273b8031f",
+    "url": "./static/css/main.fe57343a.chunk.css"
+  },
+  {
+    "revision": "ae80f55cc7377267c7df",
+    "url": "./static/js/2.1cabb15e.chunk.js"
+  },
+  {
+    "revision": "00b7f46e23c35a181934b0a16ae6dc1d",
+    "url": "./static/js/2.1cabb15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2137958604273b8031f",
+    "url": "./static/js/main.054f9e52.chunk.js"
+  },
+  {
+    "revision": "6e8b7ceada4c8a2d693c",
+    "url": "./static/js/runtime-main.4a3ee0af.js"
+  },
+  {
+    "revision": "4abc2aec9d00d77e4f7bbd30e88f4e2c",
+    "url": "./static/media/dark-horizontal-combined-rainbow.inline.4abc2aec.svg"
+  }
+]);
